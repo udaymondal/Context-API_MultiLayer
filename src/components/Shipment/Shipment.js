23e2da59eb0a4d0cc8cc2dd0dd44d456698299bd
udaymondal <udaysk.mondal@gmail.com>
2021-04-01 21:43:15 +0600

    setto shipment too

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CategoryContext } from '../../App';
 
 const Shipment = () => {
+    const [ increaseShipment, setIncreaseShipment] = useContext(CategoryContext);
     return (
         <div>
             <h3>This is shipment</h3>
+            <button onClick={() => setIncreaseShipment(increaseShipment+1)}>Increasing from shipment</button>
         </div>
     );
 };
